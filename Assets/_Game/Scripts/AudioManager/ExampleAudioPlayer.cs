66d5cd8545fdd1d3ea057bfa0d169d7d270f4ef5
cmvisaya <71780218+cmvisaya@users.noreply.github.com>
2024-06-12 17:56:24 -0500

    Updated comments

--- Assets/_Game/Scripts/AudioManager/ExampleAudioPlayer.cs
+++ Assets/_Game/Scripts/AudioManager/ExampleAudioPlayer.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//An example class of how to play background music using the Audio Manager. It will be the same for SFX.
 public class ExampleAudioPlayer : MonoBehaviour
 {
 
+    //All game objects that make sound will contain references to the sounds that they will make as fields which will be set in the inspector.
     [SerializeField] private AudioClip bgmToPlay;
-    // Start is called before the first frame update
 
+    // Start is called before the first frame update
     void Start()
     {
         if (bgmToPlay != null) AudioManager.Instance.PlayBGM(bgmToPlay);
