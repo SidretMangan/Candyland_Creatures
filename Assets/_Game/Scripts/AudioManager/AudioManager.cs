dfcc920b8416f6a4a3c71984a76e1ba9a0906dec
cmvisaya <71780218+cmvisaya@users.noreply.github.com>
2024-06-13 18:24:32 -0500

    Make transition between bgm's more seamless
    - The music now fades in and out instead of cutting to the alternate versions.
    - Added comments.
    - The "Example Audible Object" Game Object has now changed to "Example SFX Player" which now demonstrates how to play a sound effect instead of background music.

--- Assets/_Game/Scripts/AudioManager/AudioManager.cs
+++ Assets/_Game/Scripts/AudioManager/AudioManager.cs
@@ -13,47 +13,51 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     //Singleton declaration.
-    //IMPORTANT: Replace with alternate Singleton architecture eventually (by attaching SingletonDontDestroy script to AudioManager GameObject)
+    //IMPORTANT: We may replace this with alternate Singleton architecture eventually (by attaching SingletonDontDestroy script to AudioManager GameObject)
     public static AudioManager Instance { get; private set;}
 
     //BGM source loops and is the top AudioSource component in the AudioManager GameObject, SFX source does not loop
+    //The tense, neutral, and happy source are for the 3 smooth transition sources
     [SerializeField] private AudioSource bgmSource, sfxSource, tenseSource, neutralSource, happySource;
 
+    // The below fields are used for the smooth transition between tenseSource, neutralSource, and happySource
     private AudioSource[] levelAudioSources = new AudioSource[3];
     [SerializeField] private int currentSourceID = 0;
 
     private void Awake() {
 
-        //Code to generate singleton.
-        //IMPORTANT: Replace with alternate Singleton architecture eventually (by attaching SingletonDontDestroy script to AudioManager GameObject)
+        // Code to generate singleton.
+        // IMPORTANT: We may replace this with the alternate Singleton architecture eventually (by attaching SingletonDontDestroy script to AudioManager GameObject)
         if (Instance != null && Instance != this) {
             Destroy(this);
         } else { 
             Instance = this;
         }
 
-        if (bgmSource == null) {
-            Debug.LogError("bgmSource not found!");
-        }
-
-        if (sfxSource == null) {
-            Debug.LogError("sfxSource not found!");
-        }
+        // If the appropriate audio sources were not set in the inspector, we should log an error.
+        if (bgmSource == null) Debug.LogError("bgmSource not found!");
+        if (sfxSource == null) Debug.LogError("sfxSource not found!");
+        if (tenseSource == null) Debug.LogError("tenseSource not found!");
+        if (neutralSource == null) Debug.LogError("neutralSource not found!");
+        if (happySource == null) Debug.LogError("happySource not found!");
     }
 
     private void Start() {
+        // Initializes array of smooth transition audio sources
         levelAudioSources[0] = tenseSource;
         levelAudioSources[1] = neutralSource;
         levelAudioSources[2] = happySource;
     }
 
-    //Plays the given AudioClip as looping background music. Will replace any BGM that is already playing.
+    // Plays the given AudioClip as looping background music. Will replace any BGM that is already playing.
+    // IMPORTANT: Uses bgmSource, which is *different* than the three sources used for the level BGM's that are smoothly transitioned between.
     public void PlayBGM(AudioClip clip) {
         bgmSource.Stop();
         bgmSource.clip = clip;
         bgmSource.Play();
     }
 
+    // Stops all audio sources.
     public void StopAll() {
         bgmSource.Stop();
         sfxSource.Stop();
@@ -62,46 +66,85 @@ public class AudioManager : MonoBehaviour
         happySource.Stop();
     }
 
+    /*
+     * Starts playing BGM that can be smoothly transitioned between, given the three parameters.
+     * Assumes that all three AudioClips provided as parameters are of the same length and can be played simultaneously.
+     * Assumes that the "tense" clip is always the first clip to be played.
+     */
     public void PlayLevelMusic(AudioClip tenseClip, AudioClip neutralClip, AudioClip happyClip) {
+        // Stops bgmSource unrelated to smooth transition sources
         bgmSource.Stop();
 
+        // Sets the clips of the appropriate sources to the audio clips provided in the parameters
         tenseSource.clip = tenseClip;
         neutralSource.clip = neutralClip;
         happySource.clip = happyClip;
 
+        // Sets the tense source volume to 1, all others to 0
         tenseSource.volume = 1;
         neutralSource.volume = 0;
         happySource.volume = 0;
 
+        // Plays all three sources simultaneously
         tenseSource.Play();
         neutralSource.Play();
         happySource.Play();
 
+        // Sets currentSourceID to 0: tense = 0, neutral = 1, happy = 2
+        // This corresponds with how they were initialized in the Start() function
         currentSourceID = 0;
     }
 
+    /*
+     * Transitions currently playing level music to the next appropriate bgm track.
+     * Order of transition is: Tense > Neutral > Happy
+     */
     public void TransitionLevelMusic() {
         currentSourceID = (currentSourceID + 1) % levelAudioSources.Length;
+        TransitionLevelMusic(currentSourceID);
+    }
+
+    /*
+     * Transitions currently playing level music to the given sourceID's track
+     * Order of sourceID's is: 0 = Tense > 1 = Neutral > 2 = Happy
+     */
+    public void TransitionLevelMusic(int sourceID) {
         for(int i = 0; i < levelAudioSources.Length; i++) {
-            if(i == currentSourceID) {
-                levelAudioSources[i].volume = 1;
+            if(i == sourceID) {
+                StartCoroutine(StartFade(levelAudioSources[i], 0.5f, 1));
             } else {
-                levelAudioSources[i].volume = 0;
+                StartCoroutine(StartFade(levelAudioSources[i], 0.5f, 0));
             }
         }
+        
+    }
+
+    // Helper method to smoothly fade a given audio source's volume to the given volume over the given duration, in seconds.
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
     
-    //Stops the currently playing BGM, leaving silence.
+    // Stops the currently playing BGM, leaving silence.
+    // IMPORTANT: We should probably use StopAll() to stop sounds, and not this function.
     public void StopBGM() {
         bgmSource.Stop();
     }
 
-    //Plays the given AudioClip as a sound effect once at the given volume
+    // Plays the given AudioClip as a sound effect once at the given volume
     public void PlaySFX(AudioClip clip, float volume) {
         sfxSource.PlayOneShot(clip, volume);
     }
 
-    //Plays the given AudioClip as a sound effect once at maximum volume
+    // Plays the given AudioClip as a sound effect once at maximum volume
     public void PlaySFX(AudioClip clip) {
         sfxSource.PlayOneShot(clip, 1f);
     }
--- Assets/_Game/Scripts/AudioManager/ExampleAudioPlayer.cs
+++ Assets/_Game/Scripts/AudioManager/ExampleAudioPlayer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//An example class of how to play a sound effect using the Audio Manager. It will be the same for BGM.
+// An example class of how to play a sound effect using the Audio Manager. It will be the same for BGM.
 public class ExampleAudioPlayer : MonoBehaviour
 {
 
-    //All game objects that make sound will contain references to the sounds that they will make as fields which will be set in the inspector.
+    // All game objects that make sound will contain references to the sounds that they will make as fields which will be set in the inspector.
     [SerializeField] private AudioClip sfxToPlay;
 
     // Start is called before the first frame update
