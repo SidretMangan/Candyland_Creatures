c58b56344ac8cac48257e60d0172f1a1de7c6f01
andryu14 <143534883+andryu14@users.noreply.github.com>
2024-06-17 13:03:41 -0700

    Removal of Lean Tween
    Removed  Lean Tween assets and code

--- Assets/_Game/UI/ScreenMangment/TransitionManager.cs
+++ Assets/_Game/UI/ScreenMangment/TransitionManager.cs
@@ -11,7 +11,7 @@ public class TransitionManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.scale(mainScreen, new Vector3(1.5f, 1.5f, 1.5f), 2f);
+     
     }
 
    
