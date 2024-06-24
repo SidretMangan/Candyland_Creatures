447e075eea505302839d946386155a963566b296
IntrepidNeon <58537791+IntrepidNeon@users.noreply.github.com>
2024-06-24 10:18:34 -0400

    Archived Unused Scripts

--- Assets/Match3/Scripts/UIManager.cs
+++ Assets/Match3/Scripts/UIManager.cs
@@ -6,123 +6,123 @@ using UnityEngine.UI;
 public class UIManager : Singleton<UIManager>
 {
 
-    public GameObject collectionGoalLayout;
+	public GameObject collectionGoalLayout;
 
-    public int collectionGoalBaseWidth = 125;
+	public int collectionGoalBaseWidth = 125;
 
-    CollectionGoalPanel[] m_collectionGoalPanels;
+	CollectionGoalPanel[] m_collectionGoalPanels;
 
-    // reference to graphic that fades in and out
-    public ScreenFader screenFader;
+	// reference to graphic that fades in and out
+	public ScreenFader screenFader;
 
-    // UI.Text that stores the level name
-    public Text levelNameText;
+	// UI.Text that stores the level name
+	public Text levelNameText;
 
-    // UI.Text that shows how many moves are left
-    public Text movesLeftText;
+	// UI.Text that shows how many moves are left
+	public Text movesLeftText;
 
-    // reference to three-star score meter
-    public ScoreMeter scoreMeter;
+	// reference to three-star score meter
+	public ScoreMeter scoreMeter;
 
-    // reference to the custom UI window
-    public MessageWindow messageWindow;
+	// reference to the custom UI window
+	public MessageWindow messageWindow;
 
-    public GameObject movesCounter;
+	public GameObject movesCounter;
 
-    public Timer timer;
+	public Timer timer;
 
 
-    public override void Awake()
-    {
-        base.Awake();
+	public override void Awake()
+	{
+		base.Awake();
 
-        if (messageWindow != null)
-        {
-            messageWindow.gameObject.SetActive(true);
-        }
+		if (messageWindow != null)
+		{
+			messageWindow.gameObject.SetActive(true);
+		}
 
-        if (screenFader != null)
-        {
-            screenFader.gameObject.SetActive(true);
-        }
-    }
+		if (screenFader != null)
+		{
+			screenFader.gameObject.SetActive(true);
+		}
+	}
 
-    public void SetupCollectionGoalLayout(CollectionGoal[] collectionGoals, GameObject goalLayout, int spacingWidth)
-    {
-        if (goalLayout != null && collectionGoals != null && collectionGoals.Length != 0)
-        {
-            RectTransform rectXform = goalLayout.GetComponent<RectTransform>();
-            rectXform.sizeDelta = new Vector2(collectionGoals.Length * spacingWidth, rectXform.sizeDelta.y);
+	public void SetupCollectionGoalLayout(CollectionGoal[] collectionGoals, GameObject goalLayout, int spacingWidth)
+	{
+		if (goalLayout != null && collectionGoals != null && collectionGoals.Length != 0)
+		{
+			RectTransform rectXform = goalLayout.GetComponent<RectTransform>();
+			rectXform.sizeDelta = new Vector2(collectionGoals.Length * spacingWidth, rectXform.sizeDelta.y);
 
-            CollectionGoalPanel[] panels = goalLayout.GetComponentsInChildren<CollectionGoalPanel>();
+			CollectionGoalPanel[] panels = goalLayout.GetComponentsInChildren<CollectionGoalPanel>();
 
-            for (int i = 0; i < panels.Length; i++)
-            {
-                if (i < collectionGoals.Length && collectionGoals[i] != null)
-                {
-                    panels[i].gameObject.SetActive(true);
-                    panels[i].collectionGoal = collectionGoals[i];
-                    panels[i].SetupPanel();
-                }
-                else
-                {
-                    panels[i].gameObject.SetActive(false);
-                }
-            }
-        }
-    }
+			for (int i = 0; i < panels.Length; i++)
+			{
+				if (i < collectionGoals.Length && collectionGoals[i] != null)
+				{
+					panels[i].gameObject.SetActive(true);
+					panels[i].collectionGoal = collectionGoals[i];
+					panels[i].SetupPanel();
+				}
+				else
+				{
+					panels[i].gameObject.SetActive(false);
+				}
+			}
+		}
+	}
 
-    public void SetupCollectionGoalLayout(CollectionGoal[] collectionGoals)
-    {
-        SetupCollectionGoalLayout(collectionGoals, collectionGoalLayout, collectionGoalBaseWidth);
-    }
+	public void SetupCollectionGoalLayout(CollectionGoal[] collectionGoals)
+	{
+		SetupCollectionGoalLayout(collectionGoals, collectionGoalLayout, collectionGoalBaseWidth);
+	}
 
-    public void UpdateCollectionGoalLayout(GameObject goalLayout)
-    {
-        if (goalLayout != null)
-        {
-            CollectionGoalPanel[] panels = goalLayout.GetComponentsInChildren<CollectionGoalPanel>();
+	public void UpdateCollectionGoalLayout(GameObject goalLayout)
+	{
+		if (goalLayout != null)
+		{
+			CollectionGoalPanel[] panels = goalLayout.GetComponentsInChildren<CollectionGoalPanel>();
 
-            if (panels != null && panels.Length != 0)
-            {
-                foreach (CollectionGoalPanel panel in panels)
-                {
-                    if (panel != null && panel.isActiveAndEnabled)
-                    {
-                        panel.UpdatePanel();
-                    }
-                }
-            }
-        }
-    }
+			if (panels != null && panels.Length != 0)
+			{
+				foreach (CollectionGoalPanel panel in panels)
+				{
+					if (panel != null && panel.isActiveAndEnabled)
+					{
+						panel.UpdatePanel();
+					}
+				}
+			}
+		}
+	}
 
-    public void UpdateCollectionGoalLayout()
-    {
-        UpdateCollectionGoalLayout(collectionGoalLayout);
-    }
+	public void UpdateCollectionGoalLayout()
+	{
+		UpdateCollectionGoalLayout(collectionGoalLayout);
+	}
 
-    public void EnableTimer(bool state)
-    {
-        if (timer != null)
-        {
-            timer.gameObject.SetActive(state);
-        }
-    }
+	public void EnableTimer(bool state)
+	{
+		if (timer != null)
+		{
+			timer.gameObject.SetActive(state);
+		}
+	}
 
-    public void EnableMovesCounter(bool state)
-    {
-        if (movesCounter != null)
-        {
-            movesCounter.SetActive(state);
-        }
-    }
+	public void EnableMovesCounter(bool state)
+	{
+		if (movesCounter != null)
+		{
+			movesCounter.SetActive(state);
+		}
+	}
 
-    public void EnableCollectionGoalLayout(bool state)
-    {
-        if (collectionGoalLayout != null)
-        {
-            collectionGoalLayout.SetActive(state);
-        }
-    }
+	public void EnableCollectionGoalLayout(bool state)
+	{
+		if (collectionGoalLayout != null)
+		{
+			collectionGoalLayout.SetActive(state);
+		}
+	}
 
 }
